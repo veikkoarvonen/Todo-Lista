62a8d9b836ff713d6ce39a6c552efdab8cbd7637
Veikko Arvonen <arvonenveikko@gmail.com>
2024-09-18 12:50:23 +0300

    Make MVC & Brand color

---
+++ Todo Lista/Control/AddViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AddViewController.swift
+//  Todo Lista
+//
+//  Created by Veikko Arvonen on 18.9.2024.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+    
+}
---
+++ Todo Lista/Model/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Todo Lista
+//
+//  Created by Veikko Arvonen on 18.9.2024.
+//
+
+import Foundation
+
+struct C {
+    static let brandColor = "brandColor"
+}
