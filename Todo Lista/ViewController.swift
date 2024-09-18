3b0c4ac41fed3dc384cdc5cc483608d4364e6643
Veikko Arvonen <arvonenveikko@gmail.com>
2024-09-18 12:44:10 +0300

    Initial Commit

---
+++ Todo Lista/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todo Lista
+//
+//  Created by Veikko Arvonen on 18.9.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
